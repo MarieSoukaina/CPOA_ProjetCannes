f3024ee2328253d609a632d7575399e9af9cb6ad
AntoninS <antonin.spontak@gmail.com>
2017-01-03 09:00:40 +0100

    Changement fichier liason pour connexion localhost
    Cause : panne temporaire servur iut

--- Gestion_VIP/bin/liaison.php
+++ Gestion_VIP/bin/liaison.php
@@ -1,9 +1,16 @@
 <?php
 
-
+/*
 $host ="iutdoua-web.univ-lyon1.fr";
 $user ="p1501615";
 $password="240014";
+$nombase="p1501615";
+*/
+
+
+$host ="localhost";
+$user ="root";
+$password="";
 $nombase="p1501615";
 
 ?>
