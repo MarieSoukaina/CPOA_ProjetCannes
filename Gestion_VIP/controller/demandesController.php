e1b41e38b8ca7959be4608763006ce4a9af8ef84
pierreducrozet <pierre.ducrozet@etu.univ-lyon1.fr>
2017-01-04 17:48:15 +0100

    Petite modification 2

--- Gestion_VIP/controller/demandesController.php
+++ Gestion_VIP/controller/demandesController.php
@@ -6,38 +6,37 @@
       {
   			if(isset($_POST['nomVIP']) && isset($_POST['prenomVIP']) && isset($_POST['dateNaissance']) && isset($_POST['typeVIP']) && isset($_POST['infoVIP']))
   			{
-  				$dateMauvaisFormat = $_POST['dateNaissance'];
-  				$jour = substr($dateMauvaisFormat, 0, 2);
-  				$mois = substr($dateMauvaisFormat, 3, 2);
-  				$annee = substr($dateMauvaisFormat, 6, 4);
-  				$dateNaissanceBonFormat = ''.$annee.'-'.$mois.'-'.$jour.'';
 
-  				$vm->ajouterVIP($_POST['nomVIP'], $_POST['prenomVIP'], $dateNaissanceBonFormat, $_POST['typeVIP'], $_POST['infoVIP']);
-  				echo 'Ajout effectué !'; //TODO : mieux gerer la redirection après ajout
+  				echo 'testaaaaaa!'; //TODO : mieux gerer la redirection après ajout
   			}
   			else
   			{
-  				require_once("/Views/ficheVIP/ajoutDemande.php");
+  				require_once("/Views/demandes/ajoutDemande.php");
   			}
       }
 
   /*----------------------------------------MODIFICATION D'UN VIP----------------------------------------*/
-  			elseif ($_GET["action"] == "modificationVIP")
-  			{
-  				require_once("Views/ficheVIP/modificationVIP.php");
-  			}
+  		elseif ($_GET["action"] == "modificationVIP")
+  		{
+  				echo"moooooodddd";
+  		}
 
   /*----------------------------------------SUPPRESSION D'UN VIP----------------------------------------*/
-  			elseif ($_GET["action"] == "suppressionDemande")
-  			{
-  				require_once("Views/ficheVIP/suppressionDemande.php");
-  			}
+  		elseif ($_GET["action"] == "suppressionDemande")
+  		{
+  			echo"sup";
+  		}
 
   /*----------------------------------------CONSULTER LA FICHE D'UN VIP----------------------------------------*/
-  			elseif ($_GET["action"] == "consultervip")
-  			{
+  		elseif ($_GET["action"] == "consultervip")
+  		{
   				echo "test";
-  			}
+  		}
     }
+    else
+    {
+      require_once("/Views/demandes/accueilDemande.php");
+    }
+
 
 ?>
